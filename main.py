7311e391e63a551b8f8b670f377f23069a0c6860
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:08:46 -0400

    AI-enhanced and auto-formatted code

--- main.py
+++ main.py
@@ -1,129 +1,128 @@
-import random
-import time
+Here are some optimizations for the Python script:
 
+1. Use a defaultdict instead of checking for key existence in `ExpenseTracker` and `Budget` classes. This will eliminate the need for conditionals inside the loop.
 
-class RetryError(Exception):
-    pass
+```diff
+    from collections import defaultdict
 
+    class ExpenseTracker:
+        def __init__(self):
+            self.transactions = []
+            self.categories = defaultdict(int)
 
-class RetryHelper:
-    def __init__(self, max_retries=3, delay=1):
-        self.max_retries = max_retries
-        self.delay = delay
+        def add_transaction(self, transaction):
+            self.transactions.append(transaction)
+            self.categories[transaction.category] += transaction.amount
 
-    def retry(self, func):
-        tries = 0
-        while tries < self.max_retries:
-            try:
-                return func()
-            except RetryError:
-                tries += 1
-                time.sleep(self.delay)
+        def track_spending_patterns(self):
+            total_spending = sum(self.categories.values())
 
-        raise RetryError("Exceeded maximum number of retries")
+            spending_patterns = {
+                category: (amount / total_spending) * 100
+                for category, amount in self.categories.items()
+            }
 
+            return spending_patterns
 
-class Transaction:
-    def __init__(self, category, amount):
-        self.category = category
-        self.amount = amount
+    class Budget:
+        def __init__(self, income, expenses, savings_goal):
+            self.income = income
+            self.expenses = expenses
+            self.savings_goal = savings_goal
+            self.spending_limits = defaultdict(float)
 
+        def suggest_spending_limits(self):
+            total_expenses = sum(self.expenses.values())
 
-class ExpenseTracker:
-    def __init__(self):
-        self.transactions = []
+            self.spending_limits = {
+                category: (amount / total_expenses) * self.income
+                for category, amount in self.expenses.items()
+            }
 
-    def add_transaction(self, transaction):
-        self.transactions.append(transaction)
+            return self.spending_limits
+```
 
-    def track_spending_patterns(self):
-        categories = {}
-        total_spending = 0
+2. Use list comprehension instead of for loop to simplify code and make it more efficient in `FinancialAssistant` class .
 
-        for transaction in self.transactions:
-            category = transaction.category
-            amount = transaction.amount
+```diff
+    class FinancialAssistant:
+        def __init__(self):
+            self.expense_tracker = ExpenseTracker()
+            self.budget = None
+            self.retry_helper = RetryHelper()
 
-            if category not in categories:
-                categories[category] = 0
+        def track_expenses(self):
+            transactions = [
+                Transaction("Food", 50),
+                Transaction("Rent", 1000),
+                Transaction("Transportation", 100),
+                Transaction("Food", 30),
+                Transaction("Entertainment", 80),
+            ]
 
-            categories[category] += amount
-            total_spending += amount
+            self.expense_tracker.transactions.extend(transactions)
 
-        spending_patterns = {
-            category: (amount / total_spending) * 100
-            for category, amount in categories.items()
-        }
+        def analyze_spending_patterns(self):
+            def _analyze_spending_patterns():
+                spending_patterns = self.expense_tracker.track_spending_patterns()
+                print("Spending patterns:")
+- for category, percentage in spending_patterns.items():
+- print(f"- {category}: {percentage:.2f}%")
++ print("\n".join(f"- {category}: {percentage:.2f}%" for category,
+        percentage in spending_patterns.items()))
 
-        return spending_patterns
+            self.retry_helper.retry(_analyze_spending_patterns)
 
+        def set_budget(self):
+            self.budget = Budget(
+                income=2000, expenses={"Food": 300, "Rent": 1000, "Utilities": 200}, savings_goal=5000
+            )
+    
+        def suggest_spending_limits(self):
+            def _suggest_spending_limits():
+                spending_limits = self.budget.suggest_spending_limits()
+                print("Suggested spending limits:")
+-               for category, limit in spending_limits.items():
+-                   print(f"- {category}: ${limit:.2f}")
++               print("\n".join(f"- {category}: ${limit:.2f}" for category, limit in spending_limits.items()))
+    
+            self.retry_helper.retry(_suggest_spending_limits)
+    
+        def execute_autonomous_program(self):
+            self.track_expenses()
+            self.analyze_spending_patterns()
+    
+            self.set_budget()
+            self.suggest_spending_limits()
+```
 
-class Budget:
-    def __init__(self, income, expenses, savings_goal):
-        self.income = income
-        self.expenses = expenses
-        self.savings_goal = savings_goal
+3. Use a generator expression instead of a list comprehension to save memory in `FinancialAssistant` class.
 
-    def suggest_spending_limits(self):
-        total_expenses = sum(self.expenses.values())
+```diff
+    class FinancialAssistant:
+        # ...
+    
+        def track_expenses(self):
+            transactions = (
+                Transaction("Food", 50),
+                Transaction("Rent", 1000),
+                Transaction("Transportation", 100),
+                Transaction("Food", 30),
+                Transaction("Entertainment", 80),
+            )
+    
+            self.expense_tracker.transactions.extend(transactions)
+    
+        # ...
+    
+        def analyze_spending_patterns(self):
+    -       def _analyze_spending_patterns():
+    -           spending_patterns = self.expense_tracker.track_spending_patterns()
+    -           print("Spending patterns:")
+    -           print("\n".join(f"- {category}: {percentage:.2f}%" for category, percentage in spending_patterns.items()))
+    
+    -       self.retry_helper.retry(_analyze_spending_patterns)
+    +       self.retry_helper.retry(lambda: print("Spending patterns:\n", "\n".join(f"- {category}: {percentage:.2f}%" for category, percentage in self.expense_tracker.track_spending_patterns().items())))
+```
 
-        spending_limits = {
-            category: (amount / total_expenses) * self.income
-            for category, amount in self.expenses.items()
-        }
-
-        return spending_limits
-
-
-class FinancialAssistant:
-    def __init__(self):
-        self.expense_tracker = ExpenseTracker()
-        self.budget = None
-        self.retry_helper = RetryHelper()
-
-    def track_expenses(self):
-        transactions = [
-            Transaction("Food", 50),
-            Transaction("Rent", 1000),
-            Transaction("Transportation", 100),
-            Transaction("Food", 30),
-            Transaction("Entertainment", 80),
-        ]
-
-        for transaction in transactions:
-            self.expense_tracker.add_transaction(transaction)
-
-    def analyze_spending_patterns(self):
-        def _analyze_spending_patterns():
-            spending_patterns = self.expense_tracker.track_spending_patterns()
-            print("Spending patterns:")
-            for category, percentage in spending_patterns.items():
-                print(f"- {category}: {percentage:.2f}%")
-
-        self.retry_helper.retry(_analyze_spending_patterns)
-
-    def set_budget(self):
-        self.budget = Budget(
-            income=2000, expenses={"Food": 300, "Rent": 1000, "Utilities": 200}, savings_goal=5000
-        )
-
-    def suggest_spending_limits(self):
-        def _suggest_spending_limits():
-            spending_limits = self.budget.suggest_spending_limits()
-            print("Suggested spending limits:")
-            for category, limit in spending_limits.items():
-                print(f"- {category}: ${limit:.2f}")
-
-        self.retry_helper.retry(_suggest_spending_limits)
-
-    def execute_autonomous_program(self):
-        self.track_expenses()
-        self.analyze_spending_patterns()
-
-        self.set_budget()
-        self.suggest_spending_limits()
-
-
-if __name__ == "__main__":
-    assistant = FinancialAssistant()
-    assistant.execute_autonomous_program()
+I hope these optimizations help improve the performance and readability of your Python script. Let me know if you have any further questions or concerns!
