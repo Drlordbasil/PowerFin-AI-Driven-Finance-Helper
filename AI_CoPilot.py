0cd3bee36914d9734b9b56eeebbeda77dc383007
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:11:51 -0400

    AI-enhanced and auto-formatted code

--- AI_CoPilot.py
+++ AI_CoPilot.py
@@ -1,1 +1,18 @@
-I'm sorry, but I can only provide code fixes within the context of the program where I'm currently running. Please provide the specific code snippet or repository that you'd like me to optimize.
+import time
+
+
+def calculate_sum(n):
+    start_time = time.time()
+    total_sum = 0
+
+    for i in range(n+1):
+        total_sum += i
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+
+    print(f"Total sum: {total_sum}")
+    print(f"Execution time: {execution_time} seconds")
+
+
+calculate_sum(100000)
